530497ee816ea0ca049a374f6e99e33e93503a3e
amolu <amolujagare@gmail.com>
2022-06-15 20:31:57 +0530

    Initial commit

---
+++ src/SeleniumDemo1.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class SeleniumDemo1 {
+
+    public static void main(String[] args) {
+
+      /*  System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();*/
+
+       /* System.setProperty("webdriver.gecko.driver","Drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();*/
+
+       /* System.setProperty("webdriver.opera.driver","Drivers/operadriver.exe");
+        WebDriver driver = new OperaDriver();*/
+
+        System.setProperty("webdriver.edge.driver","Drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
+
+    }
+}
