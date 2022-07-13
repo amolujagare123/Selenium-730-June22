a10406e10e6f88192cb55ddab87f63ce888898ee
amolu <amolujagare@gmail.com>
2022-07-13 22:36:17 +0530

    selenium

---
+++ src/DataProviderDemo/AddCustomerExcelXHeaders2.java
@@ -0,0 +1,95 @@
+package DataProviderDemo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class AddCustomerExcelXHeaders2 {
+    WebDriver driver ;
+    @BeforeClass
+    public void doLogin()
+    {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://stock.amolujagare.com/");
+
+        WebElement txtUserName = driver.findElement(By.cssSelector("input#login-username"));
+        txtUserName.sendKeys("admin");
+
+        WebElement txtPass = driver.findElement(By.cssSelector("input[type=password]"));
+        txtPass.sendKeys("admin");
+
+        WebElement btnLogin = driver.findElement(By.cssSelector("input[value='LOG IN']"));
+        btnLogin.click();
+    }
+
+    @Test (dataProvider = "getData")
+    public void addCustomerTest(String name,String address,String contact1,String contact2)
+    {
+        WebElement lnkAddCustomer = driver.findElement(By.xpath("//a[normalize-space()='Add Customer']"));
+
+       /* JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click()",lnkAddCustomer);*/
+
+        lnkAddCustomer.click();
+
+        driver.findElement(By.name("name")).sendKeys(name);
+        driver.findElement(By.name("address")).sendKeys(address);
+        driver.findElement(By.name("contact1")).sendKeys(contact1);
+        driver.findElement(By.name("contact2")).sendKeys(contact2);
+
+        driver.findElement(By.name("Submit")).click();
+    }
+
+
+
+
+
+    @DataProvider
+    public Object[][] getData() throws IOException {
+
+        // 1. read the file
+       // FileInputStream fis = new FileInputStream("Data/MyDataX.xlsx");
+        FileInputStream fis = new FileInputStream("Data/MyDataXX-2.xlsx");
+        // 2. convert this file object into a workbook object
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        // 3. pick up the sheet where the data is
+        XSSFSheet sheet = workbook.getSheet("Add customer");
+        // 4. count the active rows in this sheet
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = sheet.getRow(0).getLastCellNum();
+
+        Object[][] data = new Object[rowCount-1][colCount];
+        for(int i=0;i<rowCount-1;i++)
+        {
+            XSSFRow row = sheet.getRow(i+1);
+
+          for (int j=0;j<colCount;j++) {
+
+              XSSFCell cell = row.getCell(j);
+              if (cell == null)
+                  data[i][j] = "";
+              else
+                  data[i][j] = cell.toString();
+          }
+
+        }
+        return data;
+    }
+}
--- src/DataProviderDemo/LoginExcelXHeaders.java
+++ src/DataProviderDemo/LoginExcelXHeaders.java
@@ -1,10 +1,6 @@
 package DataProviderDemo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -20,7 +16,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 
-public class LoginDataProviderExcelX {
+public class LoginExcelXHeaders {
 
     @Test (dataProvider = "getData")
     public void loginTest1(String username,String password)
@@ -48,41 +44,30 @@ public class LoginDataProviderExcelX {
         // 1. read the file
        // FileInputStream fis = new FileInputStream("Data/MyDataX.xlsx");
         FileInputStream fis = new FileInputStream("Data/MyDataXX-2.xlsx");
-
         // 2. convert this file object into a workbook object
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
-
         // 3. pick up the sheet where the data is
-        XSSFSheet sheet = workbook.getSheet("Sheet1");
-
+        XSSFSheet sheet = workbook.getSheet("Sheet2");
         // 4. count the active rows in this sheet
         int rowCount = sheet.getPhysicalNumberOfRows();
 
-
-        Object[][] data = new Object[rowCount][2];
-
-        for(int i=0;i<rowCount;i++)
+        Object[][] data = new Object[rowCount-1][2];
+        for(int i=0;i<rowCount-1;i++)
         {
-            XSSFRow row = sheet.getRow(i);
+            XSSFRow row = sheet.getRow(i+1); //1
 
-
-            XSSFCell cellUsername = row.getCell(0);
+            XSSFCell cellUsername = row.getCell(0); // admin
             if (cellUsername==null)
-                data[i][0] = "";
-           else {
-            //    cellUsername.setCellType(CellType.STRING);
-                // if the cell is in format other than string and data provider code does not works
+                data[i][0] = ""; // data[1][0]
+           else
                 data[i][0] = cellUsername.toString();
-            }
 
-            XSSFCell cellPassword = row.getCell(1);
+            XSSFCell cellPassword = row.getCell(1); // admin
             if (cellPassword==null)
-                data[i][1] = "";
+                data[i][1] = ""; // data[1][1]
             else
                  data[i][1] = cellPassword.toString();
-
         }
-
         return data;
     }
 }
